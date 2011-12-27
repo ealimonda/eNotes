a7578f8b60498d6eed9d8ff40786cf426de649db
Emanuele Alimonda <emanuele@alimonda.com>
2011-12-27 18:54:44 +0100

    Created Eclipse project

---
+++ Project/src/it/unica/enotes/NoteList.java
@@ -0,0 +1,13 @@
+package it.unica.enotes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoteList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
